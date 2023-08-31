cb6acb0d94484578ed68fca9ff835b5db456a13b
farooqhaider <iamhaider.0000@gmail.com>
2023-08-31 13:25:09 +0500

    concerns added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,16 @@
  class Article < ApplicationRecord
    has_many :comments
     validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 2 }
+    validates :body, presence: true, length: { minimum: 5 }
+
+   VALID_STATUSES = ['public', 'private', 'archived']
+
+   validates :status, inclusion: { in: VALID_STATUSES }
+
+   def archived?
+     status == 'archived'
+   end
+
  end
 
 
